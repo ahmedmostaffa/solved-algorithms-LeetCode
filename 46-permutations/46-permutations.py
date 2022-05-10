3123430fc4017185b1cc1ba6e6b2be5cba7ded4d
ahmed msutafa <94404894+ahmedmostaffa@users.noreply.github.com>
2022-05-10 19:28:29 +0200

    Time: 51 ms (58.95%), Space: 14 MB (59.62%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        from itertools import permutations  
+          
+        ans=[]
+     
+        
+        permutations_str = list(permutations(nums))
+        for perm in permutations_str:
+            ans.append(perm)
+            
+        return ans
+                    
+ 
+        
