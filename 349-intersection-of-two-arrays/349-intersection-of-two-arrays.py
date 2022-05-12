3f16bb73c1f0c6916232ee2e077bfcebb8f16a56
Ahmed Mustafa <94404894+ahmedmostaffa@users.noreply.github.com>
2022-05-12 16:20:34 +0200

    Time: 65 ms (53.41%), Space: 14.3 MB (25.44%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,10 +1,22 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num_1=list(set(nums1))
+        num_2=list(set(nums2))
+        from collections import defaultdict
+        
+        mp = defaultdict(lambda:0)
         ans=[]
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]==nums2[j]:
-                    ans.append(nums1[i])
-        return list(set(ans))            
+ 
+    # Insert all array elements in hash table
+        for i in range(len(num_1)):
+            mp[num_1[i]] += 1
+        for j in range(len(num_2)):
+            mp[num_2[j]] += 1
+            
+        for key,val in mp.items():
+            if val==2:
+                ans.append(key)
+        return ans        
+                
                     
         
