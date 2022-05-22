de2a5b9fcf4ffb47ff330b1e3f1d57295b6cb9be
Ahmed Mustafa <94404894+ahmedmostaffa@users.noreply.github.com>
2022-05-22 15:48:15 +0200

    Time: 285 ms (31.43%), Space: 50.9 MB (64.11%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    
+    var str=x.toString();
+    return str==str.split('').reverse().join('')
+    
+    
+};
